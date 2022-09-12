fdf9fe9b42fd92dab1c2fa7c88ea394008470f99
ifs20043 <ifs20043@students.del.ac.id>
2022-09-12 16:12:26 +0700

    updatew3

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<!--
+    Nama    : Putri Ruth Berliana Siahaan
+    NIM     : 11S20043
+    Kelas   : 13IF2
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <img src="assets/img/profile.png" alt="foto-saya" class="img-thumbnail d-block mx-auto" width="150"/>
-    <h3 class="text-center">11S20043-Putri Ruth Berliana Siahaan</h3>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PABWE - ITDEL</title>
-    <link href="assets/vendor/bootstrap5/css/bootstrap.min.css" rel="stylesheet">  
+    <link href="assets/vendor/bootstrap5/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="continer">
@@ -16,9 +19,10 @@
 
                 <div class="list-group">
                     <li href="#" class="list-group-item list-group-item-action active" aria-current="true">
-                    <h3>Hasil Praktikum</h3>
+                        <h3>Hasil Praktikum</h3>
                     </li>
-                    <a href="week2\profile.html" class="list-group-item list-group-item-action">Minggu Ke-2</a>
+                    <a href="week2/profile.html" class="list-group-item list-group-item-action">Minggu Ke-2</a>
+                    <a href="week3/loginval.html" class="list-group-item list-group-item-action">Minggu Ke-3</a>
                     <a href="#" class="list-group-item list-group-item-action">Minggu Ke-3</a>
                     <a href="#" class="list-group-item list-group-item-action">Minggu Ke-4</a>
                 </div>
